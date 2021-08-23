f38997665e3694d61b851e51ac3be1074b4e5325
arpan <arpanmalik787898@gmail.com>
2021-08-23 10:06:21 -0700

    next phase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
     <div>
-      <h1>
-        <center>arpan maliik</center>
-      </h1>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
